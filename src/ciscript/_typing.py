ee451e80e51dc1138693f13c3a5562471c0c7617
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2022-11-18 15:57:54 -0600

    Fix TypedDict

--- src/ciscript/_typing.py
+++ src/ciscript/_typing.py
@@ -1,14 +1,20 @@
 import sys
 
+# pydantic requires TypedDict to come from typing-extensions for Python < 3.9.2
+if sys.version_info < (3, 10):
+    from typing_extensions import TypedDict
+else:
+    from typing import TypedDict
+
 if sys.version_info < (3, 9):
     from typing_extensions import Annotated, Literal
 else:
     from typing import Annotated, Literal
 
 if sys.version_info < (3, 8):
-    from typing_extensions import Protocol, TypedDict
+    from typing_extensions import Protocol
 else:
-    from typing import Protocol, TypedDict
+    from typing import Protocol
 
 
 __all__ = (
